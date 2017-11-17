237c0552a941fc17a73e4b02482c80b52e7c1bb3
Alan Liang <ariez84@gmail.com>
2017-11-17 01:11:11 -0500

    First commit

---
+++ app/data/app.js
@@ -0,0 +1,28 @@
+var friendList = [{
+    name: "",
+    profilePic: "",
+    scores: ["2", "5", "2", "4", "1", "5", "5", "3", "4", "5"]
+}, 
+{
+    name: "",
+    profilePic: "",
+    scores: ["1", "2", "1", "2", "2", "4", "1", "3", "2", "4"]
+}, 
+{
+    name: "",
+    profilePic: "",
+    scores: ["1", "5", "3", "2", "4", "3", "4", "5", "1", "3"]
+}, 
+{
+    name: "",
+    profilePic: "",
+    scores: ["1", "5", "3", "3", "4", "3", "4", "5", "4", "5"]
+}, 
+{
+    name: "",
+
+    profilePic: "",
+    scores: ["3", "4", "1", "5", "1", "3", "5", "4", "1", "5"]
+}]
+
+module.exports = friendList;
