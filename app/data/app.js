24a8e789b54e93efead4635fa2fa06de0147cf2a
Alan Liang <ariez84@gmail.com>
2017-11-17 06:24:21 -0500

    finish friens objects

--- app/data/app.js
+++ app/data/app.js
@@ -1,27 +1,27 @@
 var friendList = [{
-    name: "",
-    profilePic: "",
+    name: "James Bond",
+    profilePic: "http://www.extravaganzi.com/wp-content/uploads/2014/11/James-Bond-and-Fiat-5003.jpg",
     scores: ["2", "5", "2", "4", "1", "5", "5", "3", "4", "5"]
 }, 
 {
-    name: "",
-    profilePic: "",
+    name: "Jason Bourne",
+    profilePic: "https://betterafter50.com/wp-content/uploads/2016/08/jason-bourne.jpg",
     scores: ["1", "2", "1", "2", "2", "4", "1", "3", "2", "4"]
 }, 
 {
-    name: "",
+    name: "Natalia Romanova",
     profilePic: "",
     scores: ["1", "5", "3", "2", "4", "3", "4", "5", "1", "3"]
 }, 
 {
-    name: "",
-    profilePic: "",
+    name: "Lorraine Broughton",
+    profilePic: "https://pbs.twimg.com/profile_images/443867830081945600/g2K-9fvE.jpeg",
     scores: ["1", "5", "3", "3", "4", "3", "4", "5", "4", "5"]
 }, 
 {
-    name: "",
+    name: "Sydney Bristow",
 
-    profilePic: "",
+    profilePic: "http://images4.fanpop.com/image/photos/20500000/Sydney-sydney-bristow-20557874-1024-768.jpg",
     scores: ["3", "4", "1", "5", "1", "3", "5", "4", "1", "5"]
 }]
 
